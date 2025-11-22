26cae1497a598925e8523f118756ba0162839561
Dmitrii0777 <patrrriot991@gmail.com>
2025-11-22 22:37:45 +0200

    feat(invoice): complete InvoiceCard styling with StatusBadge

--- src/features/invoices/components/InvoiceCard.tsx
+++ src/features/invoices/components/InvoiceCard.tsx
@@ -1,10 +1,13 @@
+import StatusBadge from "../../../components/common/StatusBadge";
+import { Arrow } from "../../../components/ui/Arrow";
+
 export default function InvoiceCard() {
   return (
-    <article className="bg-white mt-10 rounded-lg p-8 shadow-sm">
+    <article className="bg-bgAlt mt-10 rounded-lg p-8 shadow-sm">
       <div
         className="
         flex flex-col gap-4 justify-between 
-        lg:flex-row lg:items-center"
+        md:flex-row lg:items-center"
       >
         <h3>
           <span>#</span>RT3080
@@ -13,8 +16,9 @@ export default function InvoiceCard() {
         <p>Jensen Huang</p>
         <p>£ 1,800.90</p>
         <div>
-          <button>paid</button>
+          <StatusBadge />
         </div>
+        <Arrow className="hidden md:inline-block" direction="left" />
       </div>
     </article>
   );
