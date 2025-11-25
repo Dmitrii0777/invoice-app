28597c2f82ff9d24c0fd015bd857913508e3ff44
Dmitrii0777 <patrrriot991@gmail.com>
2025-11-25 13:37:54 +0200

    feat(invoice): add navigation to detail page on card click

--- src/features/invoices/components/InvoiceList.tsx
+++ src/features/invoices/components/InvoiceList.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from "react-router-dom";
 import type { Invoice } from "../types/invoice.types";
 import InvoiceCard from "./InvoiceCard";
 
@@ -6,11 +7,17 @@ type InvoiceListProps = {
 };
 
 function InvoiceList({ invoices }: InvoiceListProps) {
+  const navigate = useNavigate();
+
   return (
     <ul>
       {invoices.length > 0 &&
         invoices.map((invoice) => (
-          <li key={invoice.id}>
+          <li
+            className="cursor-pointer"
+            key={invoice.id}
+            onClick={() => navigate(`/invoice/${invoice.id}`)}
+          >
             <InvoiceCard invoice={invoice} />
           </li>
         ))}
