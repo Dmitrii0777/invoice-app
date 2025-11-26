a88f80ee26d3e77269d71918acb89a70c53cbc87
Dmitrii0777 <patrrriot991@gmail.com>
2025-11-26 16:50:25 +0200

    feat(redux): setup Redux Toolkit and store configuration

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,16 +1,18 @@
+import { Provider } from "react-redux";
 import { RouterProvider } from "react-router-dom";
+import { store } from "./store";
 import { router } from "./router";
 import { ModalProvider, ModalRoot } from "../provider/modal";
-import { InvoicesProvider } from "../provider/invoices";
+// import { InvoicesProvider } from "../provider/invoices";
 
 const App = () => {
   return (
-    <InvoicesProvider>
+    <Provider store={store}>
       <ModalProvider>
         <ModalRoot />
         <RouterProvider router={router} />;
       </ModalProvider>
-    </InvoicesProvider>
+    </Provider>
   );
 };
 
